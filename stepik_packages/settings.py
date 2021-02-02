ff4015f4cf2f61579c1041ec753fb6135ed281dc
Spolox <draconvs@yandex.ru>
2021-02-02 21:21:00 +0300

    to en-US

--- stepik_packages/settings.py
+++ stepik_packages/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'UTC'
 
